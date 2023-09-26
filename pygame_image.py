1d759acb58e0b338c41da84cffff25a5c8ac6fa8
C0B20116 <c0b2011694@edu.teu.ac.jp>
2023-09-26 15:56:24 +0900

    auidabfiwu

--- pygame_image.py
+++ pygame_image.py
@@ -2,22 +2,22 @@ import pygame as pg
 import sys
 
 def main():
-    pg.display.set_caption("はじめてのPygame")
+    pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    fonto  = pg.font.Font(None, 80)
+    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+
     tmr = 0
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        
+
         tmr += 1
-        txt = fonto.render(str(tmr), True, (255, 255, 255))
-        screen.fill((0, 0, 0))
-        screen.blit(txt, [300, 200])
+        screen.blit(bg_img, [0, 0])
+
         pg.display.update()
-        clock.tick(1000)
+        clock.tick(100)
 
 
 if __name__ == "__main__":
